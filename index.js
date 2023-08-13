95c26167884ff3acf832b9b46209006a8e583f31
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-08-14 00:26:38 +0530

    Added Middleware

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const app =express();
 
 const PORT=3000;
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(logger);
 
 let blogsList=[];
-app.get('/blogs',(req,res)=>{
+
+function logger(req,res,next){
+    console.log(req.url);
+    console.log(req.body);
+    let condition = false;
+    if(condition){
+        return res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
+    next();
+}
+function isAuthenticated(req,res,next){
+    console.log("Yes user is authenticated");
+    next();
+}
+app.get('/blogs',isAuthenticated,(req,res)=>{
     return res.status(404).json({
         data:blogsList,
         success:true,
